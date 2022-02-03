23e6356ec89a283aa199efadd483ce72ae4dc078
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-02 20:52:47 -0800

    add GetDescription_SetDescription_String() Test Method

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -10,9 +10,22 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
+    [TestMethod]
+    public void GetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the Dog";
+      Item newItem = new Item(description);
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      //Assert
+      Assert.AreEqual(updatedDescription,result);
+    }
   }
 }

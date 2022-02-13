f3be76390553dfc3d2fbadeab863d73608c75882
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-13 12:46:39 -0800

    fixed indentations to match lesson

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,9 +5,9 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    private static List<Item> _instances = new List<Item> { };
+    private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
@@ -22,5 +22,6 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+
   }
 }

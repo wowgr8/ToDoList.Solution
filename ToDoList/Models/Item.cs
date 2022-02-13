889fd6a89e662a2bf79615e3135d2b47bc3b3750
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-13 12:35:46 -0800

    removed spaces

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,24 +1,26 @@
 using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description {get; set;}
-    private static List<Item> _instances = new List<Item> {};//This is a static variable.
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> { };
 
-    public Item(string description)//This is the constructor.
+    public Item(string description)
     {
       Description = description;
       _instances.Add(this);
     }
 
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }

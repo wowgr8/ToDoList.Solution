5548cafee193fc26f6a509643f4851d340ae8a3e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-13 12:50:35 -0800

    add first route Index()

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -6,6 +6,12 @@ namespace ToDoList.Controllers
   public class HomeController : Controller
   {
 
+    [Route("/")]
+    public ActionResult Index()
+    {
+      Item starterItem = new Item("Add first item to To Do List");
+      return View(starterItem);
+    }
 
   }
 }

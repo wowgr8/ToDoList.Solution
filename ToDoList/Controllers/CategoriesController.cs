4ed7db690db3a37c4fbcded884ac548f2757af30
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-22 19:46:22 -0800

    add index category to display all Category's :

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -8,5 +8,12 @@ namespace ToDoList.Controllers
   public class CategoriesController : Controller
   {
 
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+    
   }
 }

5c3f5af5a8c5e4bff011b69d4e2d0d6d13ca55ef
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-22 19:44:49 -0800

    add standard controller setup

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+
+  }
+}

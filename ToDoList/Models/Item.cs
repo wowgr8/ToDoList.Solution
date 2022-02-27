34466925f04f9f870d455f00bee25690a6470c6f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-27 12:48:04 -0800

    add new GETALL() method; this code is impractical and just for learning purposes. The goal of writing this boilerplate code now is to demonstrate what is actually happening when a .NET application interacts with a database.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -16,7 +16,25 @@ namespace ToDoList.Models
 
         public static List<Item> GetAll()
         {
-            return _instances;
+            List<Item> allItems = new List<Item> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = "SELECT * FROM items;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int itemId = rdr.GetInt32(0);
+                string itemDescription = rdr.GetString(1);
+                Item newItem = new Item(itemDescription, itemId);
+                allItems.Add(newItem);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allItems;
         }
 
         public static void ClearAll()

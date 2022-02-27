7fe0e1af35c20708226a43fc872410d59a58b8ac
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-27 12:11:58 -0800

    add DBConfiguration class for MySQLConnector

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -41,4 +41,9 @@ namespace ToDoList
       });
     }
   }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=Jalapamole11*;port=3306;database=to_do_list;";
+  }
 }

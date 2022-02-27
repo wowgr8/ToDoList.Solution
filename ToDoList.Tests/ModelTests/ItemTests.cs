dbee58abcaaea4da105e7d8930938fea05cffd43
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-27 15:31:06 -0800

    add using path for MySQL, also added DBConfiguration.ConnectionString to our to_do_list_test database in an ItemTests constructor. This overrides the DBConfiguration.ConnectionString we set in Startup.cs. This ensures our tests are connected to our test database, not our development database. The ClearAll() method isn't programmed to interact with the database yet, but will be soon

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -2,6 +2,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using ToDoList.Models;
 using System;
+using MySql.Data.MySqlClient;
+
 
 namespace ToDoList.Tests
 {
@@ -12,6 +14,11 @@ namespace ToDoList.Tests
     public void Dispose()
     {
       Item.ClearAll();
+    }
+
+    public ItemTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=Jalapamole11*;port=3306;database=to_do_list_test;";
     }
 
     [TestMethod]

c2d3d68a81dfc8264786635a90821521ed3845bd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-27 12:21:55 -0800

    Because our database will persist Items, we no longer need to store Items in the static list _instances. We'll start by removing all references to _instances from our code -this commit we removed it from the class declaration and constructor.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,13 +6,10 @@ namespace ToDoList.Models
     {
         public string Description { get; set; }
         public int Id { get; }
-        private static List<Item> _instances = new List<Item> { };
 
         public Item(string description)
         {
             Description = description;
-            _instances.Add(this);
-            Id = _instances.Count;
         }
 
         public static List<Item> GetAll()

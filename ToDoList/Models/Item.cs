f7c9c6163d3761bf34f2ba74a93a98b35a8984ea
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-01 16:30:38 -0800

    Fixed indentation and added Override built in method so application knows when we want two diff objects to be considered the same

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using MySql.Data.MySqlClient;
 
-
 namespace ToDoList.Models
 {
     public class Item
@@ -9,54 +8,71 @@ namespace ToDoList.Models
         public string Description { get; set; }
         public int Id { get; }
 
+        public Item(string description)
+        {
+        Description = description;
+        }
         public Item(string description, int id)
         {
-            Description = description;
-            Id = id;
+        Description = description;
+        Id = id;
+        }
+
+        public override bool Equals(System.Object otherItem)
+        {
+        if (!(otherItem is Item))
+        {
+            return false;
+        }
+        else
+        {
+            Item newItem = (Item) otherItem;
+            bool descriptionEquality = (this.Description == newItem.Description);
+            return descriptionEquality;
+        }
         }
 
         public static List<Item> GetAll()
         {
-            List<Item> allItems = new List<Item> { };
-            MySqlConnection conn = DB.Connection();
-            conn.Open();
-            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = "SELECT * FROM items;";
-            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-            while (rdr.Read())
-            {
-                int itemId = rdr.GetInt32(0);
-                string itemDescription = rdr.GetString(1);
-                Item newItem = new Item(itemDescription, itemId);
-                allItems.Add(newItem);
-            }
-            conn.Close();
-            if (conn != null)
-            {
-                conn.Dispose();
-            }
-            return allItems;
+        List<Item> allItems = new List<Item> { };
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = "SELECT * FROM items;";
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        while (rdr.Read())
+        {
+            int itemId = rdr.GetInt32(0);
+            string itemDescription = rdr.GetString(1);
+            Item newItem = new Item(itemDescription, itemId);
+            allItems.Add(newItem);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return allItems;
         }
 
         public static void ClearAll()
         {
-            MySqlConnection conn = DB.Connection();
-            conn.Open();
-            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = "DELETE FROM items;";
-            cmd.ExecuteNonQuery();
-            conn.Close();
-            if (conn != null)
-            {
-            conn.Dispose();
-            }
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = "DELETE FROM items;";
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        if (conn != null)
+        {
+        conn.Dispose();
         }
-
+        }
         public static Item Find(int searchId)
         {
-            // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
-            Item placeholderItem = new Item("placeholder item");
-            return placeholderItem;
+        // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
+        Item placeholderItem = new Item("placeholder item");
+        return placeholderItem;
         }
     }
 }

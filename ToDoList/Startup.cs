60ffa0ea430199c887d31ec834ab7e04320849a3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-02 16:12:23 -0800

    Entity Framework Core Setup: removed DB Configuration class, we'll be storing our connection string in new appsettings.json file now. Also updated constructor to tell app what to do with connection string and dependencies in the Startup class. Also added Entity Framework middleware by including set; to IconfigurationRoot property and added Entity configuration code. Lastly, using directive for Entity was added to top of file

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace ToDoList
 {
@@ -12,15 +14,19 @@ namespace ToDoList
     {
       var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
+          .AddJsonFile("appsettings.json");
       Configuration = builder.Build();
     }
 
-    public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; set;}
 
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
     public void Configure(IApplicationBuilder app)
@@ -40,10 +46,5 @@ namespace ToDoList
         await context.Response.WriteAsync("Hello World!");
       });
     }
-  }
-
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=Jalapamole11*;port=3306;database=to_do_list;";
   }
 }

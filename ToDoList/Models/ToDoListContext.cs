b48932d120853c1094795eba52735bf19a57046a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-06 12:15:56 -0800

    add new DbSet for categories database we just created in MySQLWorkbench and OnConfiguring method to enable lazy-loading.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,7 +5,13 @@ namespace ToDoList.Models
   public class ToDoListContext : DbContext
   {
     public DbSet<Item> Items { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }  
   }
 }

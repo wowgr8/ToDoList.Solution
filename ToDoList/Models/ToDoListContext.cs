4dd51cc81785f13b529f7763ad6dbf474ef11e94
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-13 10:59:22 -0700

    Many-to-Many setup: Each DbSet we've included will become a table in our database. We've previously declared both Categories and Items DbSets, but now include a CategoryItem DbSet as well that represents our join table.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,14 +4,15 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
-    public DbSet<Item> Items { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<CategoryItem> CategoryItem { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
-    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseLazyLoadingProxies();
-    }  
+    }
   }
 }

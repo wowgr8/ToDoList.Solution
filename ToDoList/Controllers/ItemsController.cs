a6cc2b2d21437c8e18fe502c64afa8b0f07f4e50
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-13 12:58:00 -0700

    add Post Create() route. First line adds the new item to the db, then save otherwise the local item object will not have an id to enter tinto the join entity. Then add conditional to handle cases where a CategoryId doesn't get passed in to the route (such as when there are no Categories). Then inside the if block creates the association between the newly created Item and a Category. Because the Item has been added and a new ItemId has been assigned, we can create a new CategoryItem join entity. This combines the ItemId with the CategoryId specified in the dropdown menu and passed in through our route's parameters. Finally we savechanges() and then redirect to a chosen action

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -28,13 +28,18 @@ namespace ToDoList.Controllers
       return View();
     }
 
-    // [HttpPost]
-    // public ActionResult Create(Item item)
-    // {
-    //   _db.Items.Add(item);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Create(Item item, int CategoryId)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      if (CategoryId != 0)
+      {
+          _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+          _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
 
     public ActionResult Details(int id)
     {

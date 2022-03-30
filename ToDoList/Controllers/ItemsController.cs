eefb9d1de527c5c0bbc7e55cd7cc4a1febbf5b35
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-29 18:54:58 -0700

    add new using statements. Microsoft.AspNetCore.Authorization will allow us to actually authorize users, Microsoft.AspNetCore.Identity so our controller can interact with users from the database, System.Threading.Tasks will be necessary to call async methods, System.Security.Claims is important for using claim based authorization. A claim is a form of user identification. It states who a user is, not what the user can actually do. While the user identification itself doesn't authorize a user to do anything, it is necessary to first identify a user (through a claim) in order to determine whether they should be authorized.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 
 namespace ToDoList.Controllers

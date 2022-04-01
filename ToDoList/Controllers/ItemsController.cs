053a90163cef2e574ae9408d6d3efac139e216e7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-31 19:47:57 -0700

    add comments explaining Authorization setup

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -12,22 +12,33 @@ using System.Security.Claims;
 
 namespace ToDoList.Controllers
 {
-  [Authorize]
+  [Authorize] // this allows acces to the ItemsController only if a user is logged in. In this scenario, the entirety of the controller is shielded from unauthorized users. We can negate this by including an [AllowAnonymous] attribute above any specific methods that we want unauthorized users to have access to. For example, we could put [AllowAnonymous] above the Index route, if we want users to be able to see a list of items, but require authorization before they view details. 
   public class ItemsController : Controller
   {
     private readonly ToDoListContext _db;
-    private readonly UserManager<ApplicationUser> _userManager; 
+    private readonly UserManager<ApplicationUser> _userManager; // We need an instance of UserManager to work with signed-in users.
 
+    //also included a constructor to instantiate private readonly instances of the database and the UserManager.
     public ItemsController(UserManager<ApplicationUser> userManager, ToDoListContext db)
     {
       _userManager = userManager;
       _db = db;
     }
 
+    //We start by using the async modifier because this action will run asynchronously. Because the action is asynchronous, it also returns a Task containing an action result.
     public async Task<ActionResult> Index()
     {
+      // we locate the unique identifier for the currently-logged-in user and assign it the variable name userId. [this] refers to the ItemController itself. FindFirst() is a method that locates the first record that meets the provided criteria.
+      //This method takes ClaimTypes.NameIdentifier as an argument. This is why we need a using directive for System.Security.Claims. We specify ClaimTypes.NameIdentifier to locate the unique ID associated with the current account. NameIdentifier is a property that refers to an Entity's unique ID.
+      //Finally, we include the ? operator, this is called an existential operator. It states that we should only call the property to the right of the ? if the method to the left of the ? doesn't return null. Essentially, the code states that if this.User.FindFirst(ClaimTypes.NameIdentifier) returns null, don't call the property to the right of the existential operator. However, if it doesn't return null, it retrieves Value property.
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      // Once we have the userId value, we're ready to call our async method
+      //First we call the UserManager service that we've injected into this controller. We call the FindByIdAsync() method, which is a built-in Identity method used to find a user's account by their unique ID. Then we provide the userId we just located as an argument to FindByIdAsync()
+      //because [Async] suffix in this methods name, we know it runs asynchronously. We include the await keyword so the code will wait for Identity to locate the correct user before moving on.
       var currentUser = await _userManager.FindByIdAsync(userId);
+      //Finally, we create a variable to store a collection containing only the Items that are associated with the currently-logged-in user's unique Id property
+      //We use the Where() method, which is a LINQ method we can use to query a collection in a way that echoes the logic of SQL. We can use Where() to make many different kinds of queries, as the method accepts an expression to filter our results.
+      //In this case, we're simply asking Entity to find items in the database where the user id associated with the item is the same id as the id that belongs to the currentUser. This ensures users only see their own tasks in the view.
       var userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id).ToList();
       return View(userItems);
     }
@@ -41,9 +52,11 @@ namespace ToDoList.Controllers
     [HttpPost]
     public async Task<ActionResult> Create(Item item, int CategoryId)
     {
+      //The first two lines of this action are exactly the same as the first two lines of our Index() action. We start by finding the value of the current user. Then we associate the current user with the Item's User property. This makes the association so that an Item belongs to a User
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
       item.User = currentUser;
+      // Finally we add the item to the database and save it.
       _db.Items.Add(item);
       _db.SaveChanges();
       if (CategoryId != 0)
